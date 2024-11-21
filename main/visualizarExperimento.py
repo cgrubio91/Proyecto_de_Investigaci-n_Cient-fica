b92de1d79d7fb64a22f43baed6d0fa32b4936683
Wen-To <wendylo151213@gmail.com>
2024-11-20 19:58:32 -0500

    Función visualizarExperimento

---
+++ main/visualizarExperimento.py
@@ -0,0 +1,16 @@
+#Funcion para visualizar experimentos
+
+def visualizarExperimentos (listaExperimentos):
+    if not listaExperimentos:
+        print ("No hay experimentos registrados")
+        return
+    for i, experimento in enumerate(listaExperimentos, start=1):
+        print(f"\nExperimento {1}")
+        print(f"Experimento: {experimento.nombreExperimento}")
+        print(f"Fecha: {experimento.fechaExperimento.strfime('%d/%m/%Y')}")
+        print(f"Categoria: {experimento.categorias}")
+        print(f"Resultados: {experimento.resultados}")
+
+        
+
+ 
